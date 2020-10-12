f719e61e92e6a9d6582ca272c9d77e8f0130884e
DieWand <wand200.nd@gmail.com>
2020-10-12 15:14:14 +0200

    Add files via upload

---
+++ cgtbot.py
@@ -0,0 +1,66 @@
+import os
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = ".")
+
+gameemojis = ["valorant", "tft", "smash", "skyrim", "rocketleague", "r6", "minecraft", "lol", "forzahorizon", "fallguys", "escapefromtarkov", "dbd", "csgo", "civ6", "amongus"]
+gameroles = ["Valorant", "Team Fight Tactics", "Smash Bros Brawl", "Skyrim", "Rocket League", "Rainbow Six Siege", "Minecraft", "League of Legends", "Forza Horizon", 
+            "Fall Guys", "Escape from Tarkov", "Dead by Daylight", "CS:GO", "Civilization VI", "Among Us"]
+
+@bot.event
+async def on_ready():
+    print("started")
+
+@bot.command(name='start', help='start')
+async def react(ctx):
+    message = await ctx.send("Roles here")
+    for emoji in ctx.guild.emojis:
+        await message.add_reaction(emoji)
+
+@bot.command(name="members", help="members")
+async def members(ctx):
+    await ctx.send(ctx.author)
+    for member in ctx.guild.members:
+        await ctx.send(member)
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+    for gameemoji, gamerole in zip(gameemojis, gameroles):
+        if payload.emoji.name == gameemoji:
+            role = discord.utils.get(guild.roles, name=gamerole)
+            if role is not None:
+                if payload.member != bot.user:
+                    if payload.member is not None:
+                        await payload.member.add_roles(role)
+                        print("Role: \"{}\" added to {}".format(gamerole, payload.member.name))
+                    else:
+                        print("Member not found")
+            else:
+                print("Role not found")
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    guild_id = payload.guild_id
+    guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+    for gameemoji, gamerole in zip(gameemojis, gameroles):
+        if payload.emoji.name == gameemoji:
+            role = discord.utils.get(guild.roles, name=gamerole)
+            if role is not None:
+                if payload.member != bot.user:
+                    if payload.member is not None:
+                        await payload.member.remove_roles(role)
+                        print("Role: \"{}\" added to {}".format(gamerole, payload.member.name))
+                    else:
+                        print("Member not found")
+            else:
+                print("Role not found")
+  
+
+
+bot.run(Hier Bot Token platzieren)
+
+
